0f957e59590a141e8b7c423b434de38b45ba7b96
kaki1 <kakigakki@gmail.com>
2020-04-30 19:19:57 +0900

    1

--- javascript30/Webcam Fun 20200429/script.js
+++ javascript30/Webcam Fun 20200429/script.js
@@ -8,11 +8,13 @@
     const inputs = document.querySelectorAll("input[type='range']")
     let img = document.querySelector("img")
     let originImg
+        //声明rgb在图片数组中的最初的位置
     let startRGB = {
-        "r": 0,
-        "g": 1,
-        "b": 2
-    }
+            "r": 0,
+            "g": 1,
+            "b": 2
+        }
+        //声明色彩范围
     let filter = {
         "rmin": 0,
         "rmax": 255,
@@ -21,16 +23,19 @@
         "bmin": 0,
         "bmax": 255
     }
+
     let antiNameList = {
-        "rmin": "rmax",
-        "rmax": "rmin",
-        "gmin": "gmax",
-        "gmax": "gmin",
-        "bmin": "bmax",
-        "bmax": "bmin"
-    }
+            "rmin": "rmax",
+            "rmax": "rmin",
+            "gmin": "gmax",
+            "gmax": "gmin",
+            "bmin": "bmax",
+            "bmax": "bmin"
+        }
+        //声明canvas的大小，不需要加px
     canvas.height = "200"
     canvas.width = "300"
+        //获取用户摄像头的方法。返回的是一个promise对象，所以可以直接用then
     navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 200 } }).then(function(mediaStream) {
         video.srcObject = mediaStream
         video.onloadedmetadata = function() {
@@ -50,11 +55,14 @@
 
     inputs.forEach(item => {
         item.onchange = function(event) {
+            //获取最初的照片色彩
             ctx.putImageData(originImg, 0, 0);
+            //拿到照片的色彩范围
             let tempFilter = checkFilter(event.target.name, event.target.value);
             const filterMin = tempFilter.min;
             const filterMax = tempFilter.max;
             console.log(filter);
+            //重新声明新照片，用来暂时覆盖掉原始照片
             let img = ctx.getImageData(0, 0, 300, 200)
             for (let i = startRGB[event.target.name[0]]; i < img.data.length; i += 4) {
                 if (img.data[i] < filterMin) {
